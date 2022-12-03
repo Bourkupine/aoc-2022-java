d43a25fdef2814c806a858fb1a1943d16004a329
Bourkupine <euanbourke@gmail.com>
2022-12-03 13:13:19 +0000

    day03 complete

--- src/main/java/aoc/Main.java
+++ src/main/java/aoc/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         new Year2022_Day01().run(true);
         new Year2022_Day02().run(true);
+        new Year2022_Day03().run(true);
     }
 }
---
+++ src/main/java/aoc/Year2022_Day03.java
@@ -0,0 +1,69 @@
+package aoc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Year2022_Day03 extends Base<ArrayList<String>> {
+
+    void generator() {
+        this.processed = new ArrayList<>();
+        this.processed.addAll(Arrays.asList(this.input.split("\n")));
+    }
+
+
+    void part1() {
+
+        int total = 0;
+        //split strings in half
+        //compare each character in each
+        //if character is in both, convert to number
+        //add to total
+
+        for (String value : this.processed) {
+            String a = value.substring(0, value.length() / 2);
+
+            ArrayList<String> b = new ArrayList<>();
+            Collections.addAll(b, value.substring(value.length() / 2).split(""));
+
+            for (String s : a.split("")) {
+                if (b.contains(s)) {
+                    if (s.equals(s.toLowerCase())) {
+                        total += s.charAt(0) - 96;
+                    } else {
+                        total += s.charAt(0) - 38;
+                    }
+                    break;
+
+                }
+            }
+        }
+        System.out.println(total);
+    }
+
+    void part2() {
+
+        int total = 0;
+
+        for(int i = 0; i < this.processed.size();i+=3) {
+            String a = this.processed.get(i);
+            String b = this.processed.get(i + 1);
+            String c = this.processed.get(i + 2);
+
+            for (String s : a.split("")) {
+                if (b.contains(s)) {
+                    if (c.contains(s)) {
+                        if (s.equals(s.toLowerCase())) {
+                            total += s.charAt(0) - 96;
+                        } else {
+                            total += s.charAt(0) - 38;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println(total);
+
+    }
+}
